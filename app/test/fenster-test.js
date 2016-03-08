98173d4150f92f5413044f3e377cb7a0b9c95b63
fabianonunes <fabiano@nunes.me>
2016-03-08 10:09:50 -0300

    atualizar testes

--- app/test/fenster-test.js
+++ app/test/fenster-test.js
@@ -3,6 +3,8 @@
 'use strict'
 
 var $ = require('jquery')
+var responses = require('./fixtures/responses')
+
 jasmine.getFixtures().fixturesPath = 'base/app/test/fixtures/'
 
 var fenster = require('../modules/fenster.js')
@@ -16,99 +18,90 @@ describe('<fenster>', function () {
     loadFixtures('markup.html')
     $fenster = $('#page1')
     component = fenster($fenster)
+    jasmine.Ajax.install()
+  })
+
+  afterEach(function () {
+    jasmine.Ajax.uninstall()
   })
 
   describe('antes do primeiro fetch', function () {
-
     it('deve retornar o component depois da inicialização', function () {
       expect(component).toBeDefined()
     })
-
     it('deve ficar em branco', function () {
       expect($fenster).toBeEmpty()
     })
-
     it('não deve possuir altura', function () {
       expect($fenster.height()).toBe(0)
     })
-
     it('deve ter a largura do parent', function () {
       expect($fenster.width()).toBe($fenster.parent().width())
     })
-
   })
 
-  describe('fetch', function () {
+  describe('depois do fetch', function () {
 
     var mostRecentRequest = function () {
       return jasmine.Ajax.requests.mostRecent()
     }
 
-    beforeEach(function () {
-      jasmine.Ajax.install()
-    })
-
-    afterEach(function () {
-      jasmine.Ajax.uninstall()
-    })
-
-    describe('fetch', function () {
-
+    describe('quando houver uma requisição', function () {
       var request
-
       beforeEach(function () {
         component.fetch()
-        request = jasmine.Ajax.requests.mostRecent()
+        request = mostRecentRequest()
       })
 
-      describe('quando houver sucesso na requisição', function () {
-
+      describe('com sucesso', function () {
         beforeEach(function () {
-          request.respondWith({
-            status: 200,
-            contentType: 'text/html;charset=UTF-8',
-            responseText: '<b>oi</b>'
-          })
+          request.respondWith(responses.page1)
         })
 
         it('deve fazer uma requisição em data-url', function () {
           expect(request.url).toBe($fenster.data('url'))
           expect(request.method).toBe('GET')
         })
-
         it('deve renderizar texto de resposta da request dentro do component', function () {
-          expect($fenster).toContainHtml(request.responseText)
+          expect($fenster).toContainHtml('page1')
         })
-
-        it('deve limpar o componente ao setar o source vazio', function () {
-          component.setSrc('')
-          expect($fenster).toBeEmpty()
-        })
-
-        it('deve setar o valor do parametro "url" da função no source', function () {
-          component.setSrc('/page1.html')
-          expect($fenster).toContainHtml('<b>oi/b>')
-        })
-
       })
 
-      describe('quando houver erro na requisição', function () {
-
+      describe('com erro', function () {
         beforeEach(function () {
-          $fenster.html('stub')
-          request.respondWith({
-            status: 500,
-            contentType: 'text/html;charset=UTF-8',
-            responseText: '<b>Erro 500</b>'
-          })
+          request.respondWith(responses.error)
         })
 
         it('deve limpar o component', function () {
           expect($fenster).toBeEmpty()
         })
+      })
+    })
 
+    describe('ao setar o atributo src', function () {
+      beforeEach(function () {
+        $fenster.html('stub')
       })
 
+      describe('vazio', function () {
+        beforeEach(function () {
+          component.fetch = jasmine.createSpy('fetch')
+          component.setSrc('')
+        })
+
+        it('deve limpar o componente', function () {
+          expect($fenster).toBeEmpty()
+        })
+        it('não deve chamar o método fetch', function () {
+          expect(component.fetch).not.toHaveBeenCalled()
+        })
+      })
+
+      it('deve recarregar ao alterar o valor', function () {
+        component.setSrc('/page1.html')
+        mostRecentRequest().respondWith(responses.page1)
+        expect($fenster).toContainHtml('page1')
+      })
     })
 
     describe('quando houver múltiplas requisições', function () {
@@ -119,26 +112,18 @@ describe('<fenster>', function () {
 
         var firstRequest = mostRecentRequest()
         setTimeout(function () {
-          firstRequest.respondWith({
-            status: 200,
-            contentType: 'text/html;charset=UTF-8',
-            responseText: '<b>primeira</b>'
-          })
+          firstRequest.respondWith(responses.page1)
         }, 100)
 
         component.fetch()
 
         var secondRequest = mostRecentRequest()
         setTimeout(function () {
-          secondRequest.respondWith({
-            status: 200,
-            contentType: 'text/html;charset=UTF-8',
-            responseText: '<b>segunda</b>'
-          })
+          secondRequest.respondWith(responses.page2)
         }, 50)
 
         setTimeout(function () {
-          expect('<b>segunda</b>').toBe($fenster.html())
+          expect('page2').toBe($fenster.html())
           done()
         }, 150)
 
---
+++ app/test/fixtures/responses.js
@@ -0,0 +1,17 @@
+module.exports = {
+  page1 : {
+    status: 200,
+    contentType: 'text/html;charset=UTF-8',
+    responseText: 'page1'
+  },
+  page2 : {
+    status: 200,
+    contentType: 'text/html;charset=UTF-8',
+    responseText: 'page2'
+  },
+  error : {
+    status: 500,
+    contentType: 'text/html;charset=UTF-8',
+    responseText: 'error'
+  },
+}
