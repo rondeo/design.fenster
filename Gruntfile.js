410a3aec87d429fa75d6a61511382e166213e1ee
fabianonunes <fabiano@nunes.me>
2016-03-21 16:15:03 -0300

    simplificar pacote

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,12 @@
-/*jshint node:true*/
-
 'use strict';
 
 module.exports = function (grunt) {
-
-    require('load-grunt-config')(grunt, {
-      data : {
-        config: {
-          app: 'app',
-          dist: 'dist',
-          fonts: ['senado.css', 'bootstrap']
-        }
+  require('load-grunt-config')(grunt, {
+    data : {
+      config: {
+        app: 'app',
+        dist: 'dist'
       }
-    })
-
-    grunt.registerTask('dev', '--allow-remote para permitir acesso externo', function (target) {
-
-        if (grunt.option('allow-remote')) {
-            grunt.config.set('connect.options.hostname', '*')
-        }
-
-        if (grunt.option('port')) {
-            grunt.config.set('connect.options.port', grunt.option('port'))
-            grunt.config.set('connect.options.livereload', parseInt(grunt.option('port'), 10) + 1)
-        }
-
-        grunt.task.run(['_dev'])
-
-    })
-
+    }
+  })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,37 @@
 var webpack = require('webpack')
 
 module.exports = {
-    entry: {
-        'design.fenster': ['main']
-    },
-    output: {
-        path: __dirname + '/dist/scripts',
-        filename: '[name].min.js'
-    },
-    externals: {
-      jquery : 'jQuery'
-    },
-    module: {
-        loaders: [
-            { test: /\.jade$/, loader: 'jade-loader' }
-        ]
-    },
-    resolve: {
-        modulesDirectories: [
-            'node_modules',
-            __dirname + '/app/modules'
-        ]
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false,
-            mangle: true,
-            output: {
-                comments: false
-            },
-            compress : {
-                warnings: false
-            }
-        })
+  entry: {
+    'fenster': ['fenster']
+  },
+  output: {
+    path: __dirname + '/dist/scripts',
+    filename: '[name].min.js'
+  },
+  externals: {
+    jquery: 'jQuery'
+  },
+  module: {
+    loaders: [
+      { test: /\.jade$/, loader: 'jade-loader' }
     ]
+  },
+  resolve: {
+    modulesDirectories: [
+      'node_modules',
+      __dirname + '/app/modules'
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      mangle: true,
+      output: {
+        comments: false
+      },
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 }
