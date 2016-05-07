fb4dfca6908c5ef20e01428a3c4222272b281978
fabianonunes <fabiano@nunes.me>
2016-05-07 18:27:26 -0300

    bugfix: corrigir aquisição de parâmetros

--- modules/pluginify.js
+++ modules/pluginify.js
@@ -6,17 +6,19 @@ module.exports = function (pluginName, factory) {
   var old = $.fn[pluginName]
 
   $.fn[pluginName] = function (optionsOrMethod) {
+    var args = Array.prototype.slice.call(arguments, 1)
     return this.each(function () {
       var plugin = $.data(this, 'plugin-' + pluginName)
       if (!plugin) {
         plugin = Object.create(factory)
         $.data(this, 'plugin-' + pluginName, plugin.init(this, optionsOrMethod))
       } else if (plugin[optionsOrMethod]) {
-        var args = Array.prototype.slice.call(arguments, 1)
         plugin[optionsOrMethod].apply(plugin, args)
       }
     })
   }
+
+  $.fn[pluginName].factory = factory
 
   $.fn[pluginName].noConflict = function () {
     $.fn[pluginName] = old
