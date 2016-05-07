14cbd7409f75d8bda6e294bed9b5622ad6eb95aa
fabianonunes <fabiano@nunes.me>
2016-05-07 18:28:21 -0300

    reporters

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,8 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec', 'coverage'],
+    // reporters: ['spec', 'coverage'],
+    reporters: ['progress'],
 
     coverageReporter: {
       type: 'text'
