9c7e1365d11b26780f62f1f62fe61f74abea75b6
fabianonunes <fabiano@nunes.me>
2016-05-12 22:09:04 -0300

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-
 var webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
   entry: {
     fenster: ['index']
   },
   output: {
-    path: __dirname + '/dist',
+    path: path.join(__dirname, 'dist'),
     filename: '[name].min.js'
   },
   externals: {
