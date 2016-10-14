90062ed94e87dc836c9c914da96e67b98d5d6baf
fabianonunes <fabiano@nunes.me>
2016-10-14 16:23:39 -0300

    comments

--- dev/index.js
+++ dev/index.js
@@ -6,7 +6,9 @@ require('../poll')
 require('../plugin/obergaden')
 
 $('.js-fenster').fenster().on('render', function () {
-  // inicialize os outros plugins aqui
+  // inicialize os outros plugins aqui.
+  // exemplo:
+  // $(this).find('audio').audiojs()
 })
 
 $('.js-obergaden').obergaden()
